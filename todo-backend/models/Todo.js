f71dd2ff46aac146719191611be49c63a18024ab
𝕋𝔸ℕ𝕄𝕆𝕐 𝕊𝔸ℕ𝕋ℝ𝔸 <123796923+Tanmoy-Santra@users.noreply.github.com>
2025-07-16 21:30:13 +0530

    commit

--- todo-backend/models/Todo.js
+++ todo-backend/models/Todo.js
@@ -3,6 +3,7 @@ const mongoose=require('mongoose');
 const todoSchema=new mongoose.Schema({
     title:String,
     description:String,
+    //completed:boolean
 },{timestamps:true});
 
 module.exports=mongoose.model('Todo',todoSchema);
